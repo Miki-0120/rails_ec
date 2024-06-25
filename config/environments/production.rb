8d73a66ecc3f2568e6847066eed39a0ba0cc335e
Miki-0120 <miki.tanaka07@gmail.com>
2024-06-25 23:11:01 +0900

    メール機能本番環境用に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['KEY'],
+    password: ENV['SECRET_KEY'],
+    domain: 'gmail.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
