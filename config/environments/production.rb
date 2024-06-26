2e277dc60c098fdda4044e3f454836cc7837b9b7
Miki-0120 <miki.tanaka07@gmail.com>
2024-06-26 23:52:22 +0900

    production.rb修正3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'https://agile-eyrie-15031-2b777fbfdc1a.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://agile-eyrie-15031-2b777fbfdc1a.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
