6c6e8fb4c16ae45d59faa66f3d726c5d54b3dd8b
Miki-0120 <miki.tanaka07@gmail.com>
2024-07-02 22:24:18 +0900

    production.rb修正２

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
   config.action_mailer.default_url_options = { protocol: 'https',host: 'agile-eyrie-15031-2b777fbfdc1a.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
